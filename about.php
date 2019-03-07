4e7ef7cd861494f8e93d841ce2b0cbaf8f885c8e
leecodya <lee.codya@gmail.com>
2019-03-06 22:15:54 -0500

    first commit

---
+++ about.php
@@ -0,0 +1,41 @@
+<?php require_once "header.php"; ?>
+
+<ol  class = "breadcrumb">
+	<li><a href = "homepage.php">Home</a></li>
+	<li class="active">About</li>
+</ol>
+<h1 id="page-name">About</h1>
+	<div id="main">	
+		<p>	Bonica’s Pizza began operations in Roberta, Georgia in 2006 after a trip from Italy. Upon arrival, Denise, locally known as Momma, everyone’s favorite boss, brought with her so many delicious pizza recipes and combinations that she tried herself while over there. She knew from then on that she had to share the authenticity of real Italian ingredients with everyone. 
+		</p>
+		<p>	In 2008, Momma purchased Kuntry Kitchen in Roberta, Georgia from Lane Watson. Lane had spent almost 15 years building a successful business and excellent reputation in the Roberta area. Lane stayed with the company for a few years before moving on to be with his family in Kentucky. Lane and his wife, Michelle, still reside in Kentucky, and every so often, they come back down for the weekend to visit Michelle’s family here in Roberta. 
+		</p>
+		<p>	In early 2011, the restaurant was the first of its’ kind with the employment of Nikki Hightower. With Nikki’s experience as a cook at Golden Corral during the morning shift, we brought her into the family in the pursuit of incorporating breakfast into pizza! After a year of her employment, Nikki moved to Atlanta to pursue her career in nursing. 
+		</p>
+		<p>	Cheyanne Goodman was brought into the family in late 2011 due to Nikki’s pursuit of education as breakfast cook. Cheyanne received an Associate’s Degree in Culinary Arts from Fort Valley State University. Cheyanne now works independently from her home in Mississippi where she lives with her husband Jason and daughter Maria to care for her sick mother. 
+		</p>
+		<p>	In May 2014, Bonica’s Pizza adopted Mike Whelms as Restaurant Manager while Momma was at the house nursing a broken arm from a slip in the kitchen. Mike Whelms has been close friends to the family for a couple years and received a Bachelor’s Degree in Business Management from Mercer University in 2012. When Momma healed up, she saw what Mike was doing with the company and so Mike is now Restaurant Manager for a new location in Warner Robins. He and his wife Rosanne reside on the beaches of Lake Tobosofkee.  
+		</p>
+		<p>	Our most recent hire, Steven Hofton, is going our new head pizza chef. With a sharp mind, he was able to quickly pick up the different recipes and combinations in order to help Momma in the kitchen. He is taking classes at Middle Georgia State University so he may one day own his own record store.
+		</p>
+		<h1>Business Beliefs</h1>
+		<p>	Here at Bonica’s Pizza, we are all about family! We know and understand the full beneficial values that being with your family has on your well-being. We enjoy being around each other, and we love to see our customers happy, smiling, and laughing along with us. THAT’S THE ITALIAN WAY! That’s why, whenever you see us, we may bicker at each other, but we all have a certain chemistry and mind-set that makes us all on the same page. 
+		</p>
+		<h1>Current Members of the Family</h1>
+		<p>Over the years, we have had several wonderful individuals come and go from the business, but not matter what, we still consider them family and wouldn’t trade our experiences with past, present, or future employees for anything in the world! Here is a list of the current employees, including biological family, across both restaurants that we consider to be a significant part of the family:</p>
+		<ul>
+			<li>Momma (of course): Head pizza chef in Roberta, restaurant owner and manager</li>
+			<li>Boss Man (Momma’s husband): Head pizza chef in Warner Robins</li>
+			<li>Steven Hofton: Head pizza chef in training</li>
+			<li>Mike Whelms: Restaurant manager in Warner Robins</li>
+			<li>David Carpenter: Pizza Chef</li>
+			<li>Johnathon Knolton: Pizza Chef</li>
+			<li>Laura Ingle: Customer Service/waitress</li>
+			<li>Jessica Parks: Customer service/waitress</li>
+			<li>Jake Stephenson: Customer service/waiter</li>
+			<li>Lee Albritton: Customer service/waiter</li>
+		</ul>
+		<h1 id="page-name">Stop by and say hello to our wonderful and friendly staff!</h1>
+	</div>
+
+<?php require_once "footer.php"; ?>
---
+++ footer.php
@@ -0,0 +1,20 @@
+	</div><!--END CONTAINER-->
+</div><!---END WRAPPER-->	
+
+<footer>
+		
+		<section id = "myCopy">
+		<h4><a href = "site-index.php">Site Index</a></h4>
+			<h5><strong>&copy; Cody Lee<br>ITEC 2380<br>
+			Summer 2016</strong></h5>
+			
+			
+		</section>
+		
+</footer>
+	
+	<!--JavaScript Libraries go here-->
+<script src = "https://code.jquery.com/jquery-2.2.4.min.js"></script>
+				
+</body>
+</html>
